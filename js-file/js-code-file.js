fafa9ae44ff00d5e0c9465ceacd3396d5c1903ef
NagaJun1 <74446955+NagaJun1@users.noreply.github.com>
2020-12-07 22:52:59 +0900

    Rust(wasm)からJavaScriptの呼び出しを追加

---
+++ js-file/js-code-file.js
@@ -0,0 +1,5 @@
+
+export function write_p_text(a)
+{
+    document.getElementById("id_1").textContent = a;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,21 @@ use wasm_bindgen::prelude::wasm_bindgen;
 pub fn res()->String{
     format!("Rust側の返答")
 }
+
+#[wasm_bindgen]
+pub fn read_js_fnction(){
+    //jsのアラート
+    alert("call JavaScript function".to_string());
+    //js関数での処理
+    write_p_text("Rust call function".to_string());
+}
+
+#[wasm_bindgen(module="/js-file/js-code-file.js")]
+extern "C"{
+    fn write_p_text(a:String);
+}
+
+#[wasm_bindgen]
+extern "C"{
+    fn alert(text:String);
+}
